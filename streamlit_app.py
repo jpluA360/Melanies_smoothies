64095de06fe4c5ef5028e0cdc47774ebe833bcff
jpluA360 <jlemmon@accelerize360.com>
2024-12-29 18:54:08 -0500

    Update streamlit_app.py
    added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
-
+t pandas as pd
 # Write directly to the app
 st.title(":cup_with_straw: Customize you Smoothie :cup_with_straw:")
 st.write(
@@ -15,7 +15,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
-
+pd_df = my_dataframe.to_pandas()
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on you Smoothie will be: ", name_on_order)
 ingredients_list = st.multiselect('Choose up to 5 ingredients: ', my_dataframe, max_selections=5)
