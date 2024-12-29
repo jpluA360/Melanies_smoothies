f8b8125cccbc11f0805b783c3d6614d41561d9f7
jpluA360 <jlemmon@accelerize360.com>
2024-12-29 18:54:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
-t pandas as pd
+import pandas as pd
 # Write directly to the app
 st.title(":cup_with_straw: Customize you Smoothie :cup_with_straw:")
 st.write(
